c9eaa3bf75b67dfe4b6876efedcc78e706f234a3
Unheralded <jleemwaniki@gmail.com>
2023-09-08 06:24:37 +0300

    4,5,6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/**
- * main - display  "Programming is like building a multilingual puzzle"
- * Return: 0 and exit 
+/*
+ * main - display "Programming is like biulding a multilingual puzzle"
+ * Return: 0 and exit program
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like biulding a multilingual puzzle");
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - displays text with new line
+ * Return: 0 and end program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
