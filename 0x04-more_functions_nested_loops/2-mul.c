eb2f5ef3693c11dc546c114ac090597084299273
Unheralded <jleemwaniki@gmail.com>
2023-09-16 06:18:55 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - product of int a & int b
+ * 
+ * @a: int to mul
+ * @b: int to mul
+ * Return: 0 Success.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
