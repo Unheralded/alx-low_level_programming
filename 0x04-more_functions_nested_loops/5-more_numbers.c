8573728969987e10384cced34daf0d5a60c50746
Unheralded <jleemwaniki@gmail.com>
2023-09-16 07:11:50 +0300

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print product of numbers 0-14 * 10 ten times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
