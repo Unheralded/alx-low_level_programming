c24941dc27956e21e01cbbf2ab1a3827879d21ad
Unheralded <jleemwaniki@gmail.com>
2023-09-24 16:01:21 +0300

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
