55838858d21de24c6c72a15eae27a37cfcdf6949
Unheralded <jleemwaniki@gmail.com>
2023-10-04 12:43:36 +0300

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-temp_dir="temp_obj_dir"
-mkdir -p "$temp_dir"
-
-for source_file in *.c; do
-    object_file="${temp_dir}/${source_file%.c}.o"
-    gcc -c "$source_file" -o "$object_file"
-    if [ $? -ne 0 ]; then
-        echo "Compilation failed for $source_file"
-        exit 1
-    fi
-done
-
-ar rcs liball.a "${temp_dir}"/*.o
-
-rm -r "$temp_dir"
-
+gcc -c *.c -o *.o
+ar rcs liball.a *.o
 
