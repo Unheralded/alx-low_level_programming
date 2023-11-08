c6b02b72601e1f47a6dd2eeaa397e2b00270807c
Unheralded <jleemwaniki@gmail.com>
2023-11-08 05:30:21 +0300

    0, README

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+ * struct dog - dog details
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
