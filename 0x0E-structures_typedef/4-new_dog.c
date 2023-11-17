c5d3ed5f2687b3bad9832095bc0b210725c571c0
Unheralded <jleemwaniki@gmail.com>
2023-11-17 22:12:48 +0300

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: new dog name
+ * @age: new dog age
+ * @owner: new dog owner name
+ *
+ * Return: ponter to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *ptr;
+	char *i, *j;
+	int a, b, c;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(struct dog));
+	if (ptr == NULL)
+		return (NULL);
+	for (a = 0; *(a + name) != '\0'; a++)
+		;
+	for (b = 0; *(b + owner) != '\0'; b++)
+		;
+	i = malloc(a + 1);
+	j = malloc(b + 1);
+	if (i == NULL || j == NULL)
+	{
+		free(ptr);
+		free(i);
+		free(j);
+
+		return (NULL);
+	}
+	for (c = 0; c <= a; c++)
+		*(i + c) = (*name + c);
+	for (c = 0; c <= b; c++)
+		*(j + c) = (*owner + c);
+
+	ptr->name = i;
+	ptr->age = age;
+	ptr->owner = j;
+
+	return (ptr);
+}
